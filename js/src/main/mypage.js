601a44b9d59414b8219e1fde683c99e8902e1fa7
CHO KANG RAE <joh971128@gmail.com>
2021-05-10 16:29:45 +0900

    마이페이지 - 기본등록정보 - 수정

--- js/src/main/mypage.js
+++ js/src/main/mypage.js
@@ -20,6 +20,29 @@ $(document).ready(function(){
 		$('ul.tabs li').removeClass('current');
 
 		$(this).addClass('current');
-  });
-  
+	});
+	
+	// ========== 기본등록정보 - 수정 ============
+	$('.resetBtn').click(function(){
+
+		$('.myinfoBox').hide();
+		$('.profile_modify_box').show();
+
+	});
+
+	$('.profile_modify_no').click(function(){
+
+		$('.myinfoBox').show();
+		$('.profile_modify_box').hide();
+
+	});
+
+	$('.profile_modify_yes').click(function(){
+
+		$('.myinfoBox').show();
+		$('.profile_modify_box').hide();
+
+	});
+	
+
 });
