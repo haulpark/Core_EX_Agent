98cbf0cec3f4ef1d0a9b2fe7167c398e7889d9fb
CHO KANG RAE <joh971128@gmail.com>
2021-05-21 16:49:51 +0900

    open api 문구 수정 및 이용약관 수정 및 디자인 작업 추가

--- js/src/open_api/api_openKey.js
+++ js/src/open_api/api_openKey.js
@@ -11,3 +11,23 @@ $('.close').click(function(e){
 
   $('#issuedBox').hide();
 });
+
+$('.acc_scan').click(function(e){
+  e.preventDefault();
+
+  $('.scan_alarm').text("Access Key가 복사되었습니다.");
+  $('.scan_alarm').show();
+
+  var obj = document.getElementById("ac_id");
+  document.execCommand("copy");
+});
+
+$('.sec_scan').click(function(e){
+  e.preventDefault();
+
+  $('.scan_alarm').text("Secret Key가 복사되었습니다.");
+  $('.scan_alarm').show();
+
+  var obj = document.getElementById("se_id");
+  document.execCommand("copy");
+});
